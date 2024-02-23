6573fd83ca04cafe8a09d7da3c5fee540417047e
HuskyInSalt <156379027+HuskyInSalt@users.noreply.github.com>
2024-02-23 22:22:18 +0800

    Add files via upload

--- run_crag_inference.sh
+++ run_crag_inference.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 cd scripts
-CUDA_VISIBLE_DEVICES=1 python CRAG_Inference.py \
+
+dataset=popqa
+python CRAG_Inference.py \
 --generator_path YOUR_GENERATOR_PATH \
 --evaluator_path YOUR_EVALUATOR_PATH \
---input_file ../data/popqa/test_popqa.txt \
---output_file ../data/popqa/output/YOUR_OUTPUT_FILE \
---internal_knowledge_path ../data/popqa/ref/correct \
---external_knowledge_path ../data/popqa/ref/incorrect \
---combined_knowledge_path ../data/popqa/ref/ambiguous \
---task popqa --method crag --device cuda:0 \
+--input_file ../data/$dataset/test_$dataset.txt \
+--output_file ../data/$dataset/output/YOUR_OUTPUT_FILE \
+--internal_knowledge_path ../data/$dataset/ref/correct \
+--external_knowledge_path ../data/$dataset/ref/incorrect \
+--combined_knowledge_path ../data/$dataset/ref/ambiguous \
+--task $dataset --method crag --device cuda:0 \
 --ndocs 10 --batch_size 8 --upper_threshold 0.592 --lower_threshold 0.995
 
-# CUDA_VISIBLE_DEVICES=1 python CRAG_Inference.py \
+# python CRAG_Inference.py \
 # --generator_path YOUR_GENERATOR_PATH \
 # --evaluator_path YOUR_EVALUATOR_PATH \
 # --input_file ../data/pubqa/test_pubqa.txt \
