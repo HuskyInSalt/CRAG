9f410f06aa7ea1b67b2b8a6eb992942598dcb59d
HuskyInSalt <156379027+HuskyInSalt@users.noreply.github.com>
2024-03-01 21:00:38 +0800

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -1,4 +1,5 @@
+cd scripts
 python eval.py \
-  --input_file eval_data/health_claims_processed.jsonl \
+  --input_file ../eval_data/health_claims_processed.jsonl \
   --eval_file ../data/pubqa/output/YOUR_OUTPUT_FILE \
   --metric match --task fever
