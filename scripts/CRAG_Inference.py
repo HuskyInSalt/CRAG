baadc37b218aed314683fa3e923a9a29e4ff91c5
HuskyInSalt <156379027+HuskyInSalt@users.noreply.github.com>
2024-10-08 20:45:10 +0800

    Add files via upload

--- scripts/CRAG_Inference.py
+++ scripts/CRAG_Inference.py
@@ -19,6 +19,8 @@ from vllm import LLM, SamplingParams
 from transformers import T5Tokenizer, T5ForSequenceClassification
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+import time
+
 # logger = logging.getLogger(__name__)
 
 TASK_INST = {"wow": "Given a chat history separated by new lines, generates an informative, knowledgeable and engaging response. ",
@@ -36,14 +38,13 @@ control_tokens = ["[Fully supported]", "[Partially supported]", "[No support / C
 task = ### Instruction: + TASK_INST[task] + ## Input: + question + ### Response:
 task = ### Instruction: + TASK_INST[task] + ## Input: + question + choices + ### Response:
 '''
-def format_prompt(i, task, question, paragraph=None):
-    # if task == "bio":
-    #     prompt = "### Instruction:\n{0}\n\n### Response:\n".format(question)
-    #     if paragraph is not None:
-    #         prompt += "[Retrieval]<paragraph>{0}</paragraph>".format(paragraph)
-    # else:
+def format_prompt(i, task, question, paragraph=None, modelname="selfrag_llama"):
+    if paragraph is not None:
+        paragraph = ' '.join(paragraph.split(' ')[:])
+
     instruction = TASK_INST[task] if task in TASK_INST else None
     instruction = instruction + "\n\n## Input:\n\n" + question if instruction is not None else question
+
     if task == "arc_challenge":
         with open("../data/arc_challenge/choices", 'r') as f:
             choices = f.readlines()[i].strip()
@@ -54,9 +55,25 @@ def format_prompt(i, task, question, paragraph=None):
         choices = choices.replace("E: ", "\nE: ")
         instruction += choices
 
-    prompt = "### Instruction:\n{0}\n\n### Response:\n".format(instruction)
-    if paragraph is not None:
-        prompt += "[Retrieval]<paragraph>{0}</paragraph>".format(paragraph)
+    if instruction == question:
+        # PopQA
+        prompt = "Refer to the following documents, follow the instruction and answer the question.\n\nDocuments: " + paragraph + "\n\nInstruction: Answer the question: " + question
+    else:
+        if task == "arc_challenge":
+            prompt = "Refer to the following documents, follow the instruction and answer the question.\n\nDocuments: " + paragraph + "\nQuestion: " + question + "\n\nInstruction: Given four answer candidates, A, B, C and D, choose the best answer choice." + "\nChoices:" + choices 
+
+        elif task == "pubqa":
+            if modelname == "llama":
+                prompt = "Read the documents and answer the question: Is the following statement correct or not? \n\nDocuments: " + paragraph + "\n\nStatement: " + question + "\n\nOnly say true if the statement is true; otherwise say false."
+            else:
+                prompt = "### Instruction:\n{0}\n\n### Response:\n".format(instruction)
+                if paragraph is not None:
+                    prompt += "[Retrieval]<paragraph>{0}</paragraph>".format(paragraph)
+            # prompt = "Refer to the following documents, follow the instruction and answer the question.\n\nDocuments: " + paragraph + "\n\nInstruction: Is the following statement correct or not? Say true if it's correct; otherwise say false. \nStatement: " + question
+    # prompt = "### Instruction:\n{0}\n\n### Response:\n".format(instruction)
+    # if paragraph is not None:
+    #     prompt += "[Retrieval]<paragraph>{0}</paragraph>".format(paragraph)
+
     return prompt
 
 def postprocess_answer_option_conditioned(answer):
@@ -73,7 +90,7 @@ def postprocess_answer_option_conditioned(answer):
 
     return answer
 
-def data_preprocess(file):
+def data_preprocess(file, n_docs):
     # with_label = True
     with_label = False
     queries = []
@@ -81,7 +98,7 @@ def data_preprocess(file):
     tmp_psgs = []
     with open(file, "r", encoding="utf-8") as f:
         if with_label:
-            for line in f.readlines()[:]:
+            for i, line in enumerate(f.readlines()[:]):
                 c, l = line.strip().split("\t")
                 q, p = c.split(' [SEP] ')
                 if queries == []:
@@ -89,14 +106,14 @@ def data_preprocess(file):
                     tmp_psgs = [p]
                 else:
                     if q != queries[-1]:
-                        passages.append(' [sep] '.join(tmp_psgs))
+                        passages.append(' [sep] '.join(tmp_psgs[:n_docs]))
                         queries.append(q)
                         tmp_psgs = [p]
                     else:
                         tmp_psgs.append(p)
-            passages.append(' [sep] '.join(tmp_psgs))
+                passages.append(' [sep] '.join(tmp_psgs[:n_docs]))
         else:
-            for line in f.readlines():
+            for i, line in enumerate(f.readlines()):
                 c = line.strip()
                 if c.endswith('[SEP]'):
                     c += ' '
@@ -106,12 +123,12 @@ def data_preprocess(file):
                     tmp_psgs = [p]
                 else:
                     if q != queries[-1]:
-                        passages.append(' [sep] '.join(tmp_psgs))
+                        passages.append(' [sep] '.join(tmp_psgs[:n_docs]))
                         queries.append(q)
                         tmp_psgs = [p]
                     else:
                         tmp_psgs.append(p)
-            passages.append(' [sep] '.join(tmp_psgs))
+            passages.append(' [sep] '.join(tmp_psgs[:n_docs]))
     return queries, passages
 
 def get_evaluator_data(file):
@@ -138,10 +155,12 @@ def inference(tokenizer, model, file, device=torch.device("cpu"), n_docs=10):
     preds = []
     scores = []
 
-    for c in tqdm(content[:]):
+    for n, c in tqdm(enumerate(content[:])):
         if c.strip().endswith('[SEP]'):
             preds.append(-1)
             scores.append(-1.0)
+            continue
+        if n % 10 > n_docs-1:
             continue
         test = tokenizer(c, return_tensors="pt",padding="max_length",max_length=512)
         with torch.no_grad():  
@@ -201,42 +220,35 @@ def main():
     args = parser.parse_args()
     args.lower_threshold = -args.lower_threshold
 
-    # assert 1==0
+    generator = LLM(model=args.generator_path, dtype="half")
+    sampling_params = SamplingParams(temperature=0.0, top_p=1.0, max_tokens=100, skip_special_tokens=False)
+    
     tokenizer = T5Tokenizer.from_pretrained(args.evaluator_path)
     model = T5ForSequenceClassification.from_pretrained(args.evaluator_path, num_labels=1)
-
     device = torch.device(args.device) if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
 
-    # assert 1==0
-    queries, passages = data_preprocess(args.input_file)
-
-    # assert 1==0
-    scores = inference(
-        tokenizer=tokenizer, 
-        model=model, 
-        file=args.input_file,
-        device=device, 
-        n_docs=args.ndocs
-    )
-
-    # str_scores = [str(s) for s in scores]
-    # with open('../data/pubqa/score_log', 'w') as f:
-    #     f.write('\n'.join(str_scores))
-
-    # with open('../data/arc_challenge/score_log', 'r') as f:
-    #     scores = [float(l.strip()) for l in f.readlines()]
-    identification_flag = process_flag(scores, args.ndocs, args.upper_threshold, args.lower_threshold)
+    queries, passages = data_preprocess(args.input_file, args.ndocs)
 
     if args.method == 'rag':
         paragraphs = passages
     elif args.method == 'crag':
+        scores = inference(
+            tokenizer=tokenizer, 
+            model=model, 
+            file=args.input_file,
+            device=device, 
+            n_docs=args.ndocs
+        )
+        identification_flag = process_flag(scores, args.ndocs, args.upper_threshold, args.lower_threshold)
+
         with open(args.internal_knowledge_path, 'r') as in_f, open(args.external_knowledge_path, 'r') as ex_f, open(args.combined_knowledge_path, 'r') as comb_f:
             internal_paragraphs = [l.strip() for l in in_f.readlines()]
             external_paragraphs = [l.strip() for l in ex_f.readlines()]
             combined_paragraphs = [l.strip() for l in comb_f.readlines()]
 
         paragraphs = []
+        n = 0
         for flag, i, e, c in zip(identification_flag, internal_paragraphs, external_paragraphs, combined_paragraphs):
             if flag == 0:
                 paragraphs.append(e) # incorrect
@@ -244,19 +256,19 @@ def main():
                 paragraphs.append(c) # ambiguous
             elif flag == 2:
                 paragraphs.append(i) # correct
+            n += 1
     
     preds = []
-    generator = LLM(model=args.generator_path, dtype="half")
-    sampling_params = SamplingParams(temperature=0.0, top_p=1.0, max_tokens=100, skip_special_tokens=False)
+    modelname = "selfrag_llama" if "selfrag" in args.generator_path else "llama"
     if args.method != 'no_retrieval':
         for i, (q, p) in tqdm(enumerate(zip(queries, paragraphs))):
-            prompt = format_prompt(i, args.task, q, p)
+            prompt = format_prompt(i, args.task, q, p, modelname)
             pred = generator.generate([prompt], sampling_params)
             preds.append(postprocess_answer_option_conditioned(pred[0].outputs[0].text))
     else:
         for i, q in tqdm(enumerate(queries)):
             p = None
-            prompt = format_prompt(i, args.task, q, p)
+            prompt = format_prompt(i, args.task, q, p, modelname)
             pred = generator.generate([prompt], sampling_params)
             preds.append(postprocess_answer_option_conditioned(pred[0].outputs[0].text))
 
